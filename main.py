8454f59473a265c836800e5b30241b555c76bbf8
Julio Rafael <juliorafaelnft@hotmail.com>
2024-03-27 12:41:29 -0300

    Update

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def products(name=None):
     products = {
         'product1': {'name': 'Apple', 'price': 1.00, 'description': 'A juicy red apple.'},
         'product2': {'name': 'Banana', 'price': 0.50, 'description': 'A ripe yellow banana.'},
+        'product3': {'name': 'Teste', 'price': 0.20, 'description': 'Teste'},
+        
     }
     context = {
         'products': products
