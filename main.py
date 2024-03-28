13d71e42c276724246a9d3fc6884df06913df3c6
Julio Rafael <juliorafaelnft@hotmail.com>
2024-03-28 02:24:49 -0300

    Add database

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from flask import Flask, render_template
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///itens.db'
+db.init_app(app)
 
 @app.route('/')
 @app.route('/<name>')
 def index(name=None):
-    return render_template('index.html', name=name, )
+    return render_template('index.html', name=name)
 
 @app.route('/products/')
 @app.route('/products/<name>')
 def products(name=None):
-    products = {
-        'product1': {'name': 'Apple', 'price': 1.00, 'description': 'A juicy red apple.'},
-        'product2': {'name': 'Banana', 'price': 0.50, 'description': 'A ripe yellow banana.'},
-        'product3': {'name': 'Teste', 'price': 0.20, 'description': 'Teste'},
-        
-    }
     context = {
-        'products': products
+        'products': 'products'
     }
     return render_template('products.html', name=name, **context)
+
+@app.route('/db')
+def show_db():
+    users = User.query.all()
+    user_list = [str(user) for user in users]
+    return '<br>'.join(user_list)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run()
